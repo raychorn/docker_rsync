7908fc51b4984e1e5087b5510138858157e747f8
Ray C Horn <raychorn@gmail.com>
2021-12-30 14:48:32 -0700

    updates.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@ DIR0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 apt-get install cifs-utils -y
 
+# install docker command line tools
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+apt-get install docker-ce -y
+
 mkdir -p ${DEST_DIR}
 mount -t cifs -o "username=${username},password=${password}" ${DEST_SMB} ${DEST_DIR}
 
